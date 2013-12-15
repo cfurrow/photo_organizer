95c87ca10401bdc8fb67ff1987aa3ae4d5cdeb64
Carl Furrow <carl.furrow@gmail.com>
2013-12-14 21:35:36 -0800

    DDetect filename collisions and correct with incrementing index

--- lib/photo_organizer/file_mover.rb
+++ lib/photo_organizer/file_mover.rb
@@ -1,20 +1,42 @@
 require 'fileutils'
 
 class FileMover
-  attr_reader :configuration
+  attr_reader :configuration, :file_detail, :dest_folder, :file_name
 
-  def initialize(configuratio=nil)
+  def initialize(file_detail, dest_folder, configuration=nil)
     @configuration = configuration || Configuration.new
+    @file_detail   = file_detail
+    @dest_folder   = dest_folder
+    @file_name     = file_detail.new_filename
+    @unique_index  = 0
   end
 
-  def move(file_detail, dest_folder)
+  def move
     # make dir based on year/month/day (0 padded)
-    file_date = file_detail.datetime
+    file_date = @file_detail.datetime
 
     folder = File.join(File.expand_path("#{dest_folder}"), file_date.strftime(configuration.format))
     FileUtils.mkdir_p(folder)
 
-    FileUtils.mv(file_detail.full_path, "#{folder}/#{file_detail.new_filename}")
-    Logger.log("\tMoved to #{folder}/#{file_detail.new_filename}")
+    @file_name = unique_filename(@file_name, folder)
+    FileUtils.mv(file_detail.full_path, "#{folder}/#{@file_name}")
+    Logger.log("\tMoved to #{folder}/#{@file_name}")
+  end
+
+  private
+
+  def unique_filename(file_name, folder)
+    full_path = File.expand_path(File.join(folder, file_name))
+    if File.exists?(full_path)
+      @unique_index += 1
+      extension  = File.extname(full_path)
+      file_name = file_name.gsub(/(\-\d+)?#{extension}$/i,"-#{@unique_index}#{extension}")
+      Logger.log("\tFile exists. Trying #{file_name}")
+      unique_filename(file_name, folder)
+    else
+      @unique_index = 0
+      file_name
+    end
+
   end
 end
